2ae5ab4c416d0be2b8ca59d436634e7ef35de687
karantyagi <tyagi.k@husky.neu.edu>
2018-06-03 22:11:44 -0400

    added picker element

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View,ScrollView  } from 'react-native';
+import { StyleSheet, View, ScrollView  } from 'react-native';
 import { Divider } from 'react-native-elements'
 
 import FixedHeader from './elements/FixedHeader'
@@ -7,6 +7,9 @@ import TextHeadings from './elements/TextHeadings'
 import Icons from './elements/Icons'
 import Exam from './elements/Exam'
 import QuestionTypeButtonGroupChooser from './elements/QuestionTypeButtonGroupChooser'
+import QuestionTypePicker from './elements/QuestionTypePicker'
+
+import {Picker} from 'react-native'
 
 export default class App extends React.Component {
   render() {
@@ -15,6 +18,7 @@ export default class App extends React.Component {
             <FixedHeader/>
 
             <QuestionTypeButtonGroupChooser/>
+            <QuestionTypePicker/>
             <Exam/>
 
             <View style={{padding:20}}>
